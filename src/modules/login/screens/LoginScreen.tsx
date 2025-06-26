03f76f0cbdc6d33af6f5239dbbb0654eaa0c9fe0
MatheusFarias23 <belllafarias99@gmail.com>
2025-06-26 20:33:04 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles";
+
+const LoginScreen = () => {
+    return (
+        <BackgroundImage src="./background.png" />
+    );
+};
+
+export default LoginScreen;
---
+++ src/modules/login/styles/LoginScreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const BackgroundImage = styled.img`
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    object-fit: cover;
+`;
