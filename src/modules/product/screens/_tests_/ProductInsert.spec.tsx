8c32337ec74f52841e558647a5deeaca936f3152
MatheusFarias23 <belllafarias99@gmail.com>
2025-11-24 17:32:23 -0300

    feat: insert unit test in insertProduct

--- src/modules/product/screens/_tests_/ProductInsert.spec.tsx
+++ src/modules/product/screens/_tests_/ProductInsert.spec.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import ProductInsert from '../ProductInsert';
 import { ProductInsertTestIdEnum } from '../../enum/ProductInsertTestIdEnum';
 import { mockProductInsert } from '../../_mocks_/productInsert.mock';
@@ -20,20 +20,26 @@ jest.mock('../../../../shared/hooks/useRequests', () => ({
   }),
 }));
 
+let value = '';
+let type = '';
+
 jest.mock('../../hooks/useInsertProduct', () => ({
   useInsertProduct: () => ({
     loading: false,
     product: mockProductInsert,
     disabledButton: false,
     handleOnClickCancel: jest.fn(),
-    onChangeInput: jest.fn(),
+    onChangeInput: (e: React.ChangeEvent<HTMLInputElement>, x: string) => {
+      value = e.target.value;
+      type = x;
+    },
     handleChangeSelect: jest.fn(),
     handleInsertProduct: jest.fn(),
   }),
 }));
 
 describe('Test Product Insert', () => {
-  it('should render container and logo', () => {
+  it('should render', () => {
     const { getByTestId } = render(<ProductInsert />);
     waitFor(() => {
       expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_INSERT_CONTAINER)).toBeDefined();
@@ -44,6 +50,33 @@ describe('Test Product Insert', () => {
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_SELECT)).toBeDefined();
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_BUTTON_INSERT)).toBeDefined();
     expect(getByTestId(ProductInsertTestIdEnum.PRODUCT_BUTTON_CANCEL)).toBeDefined();
+  });
+
+  it('should call onChangeInput in change name', () => {
+    const { getByTestId } = render(<ProductInsert />);
+
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_NAME);
+    fireEvent.change(input, { target: { value: 'MOCK_VALUE' } });
+    expect(value).toEqual('MOCK_VALUE');
+    expect(type).toEqual('name');
+  });
+
+  it('should call onChangeInput in change price', () => {
+    const { getByTestId } = render(<ProductInsert />);
+
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_PRICE);
+    fireEvent.change(input, { target: { value: '2305' } });
+    expect(value).toEqual('23.05');
+    expect(type).toEqual('price');
+  });
+
+  it('should call onChangeInput in change image', () => {
+    const { getByTestId } = render(<ProductInsert />);
+
+    const input = getByTestId(ProductInsertTestIdEnum.PRODUCT_INPUT_IMAGE);
+    fireEvent.change(input, { target: { value: 'http-image' } });
+    expect(value).toEqual('http-image');
+    expect(type).toEqual('image');
   });
 });
 
